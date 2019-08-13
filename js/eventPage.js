45eba77db2f94e5e663c3e9dd84e45f58374d4e8
insaner <github@insaner.com>
2019-08-13 08:57:36 -0400

    add tab count badge (#11)

---
+++ js/eventPage.js
@@ -0,0 +1,17 @@
+// added from "chrome tab count"
+
+function updateIcon(tabId, changeInfo, tab) {
+    chrome.tabs.query({currentWindow: true},
+    function(tabs){
+        var ntabs = tabs.length;
+        var ntabs_str = ntabs.toString();
+	chrome.browserAction.setBadgeText({text: ntabs_str});
+    });
+}
+
+chrome.tabs.onCreated.addListener(updateIcon);
+chrome.tabs.onRemoved.addListener(updateIcon);
+chrome.tabs.onReplaced.addListener(updateIcon);
+chrome.windows.onFocusChanged.addListener(updateIcon);
+
+updateIcon();
